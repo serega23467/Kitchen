4b3fae7796972e5527523bd65dca757f1566d363
sereg <sereg.abrs@mail.ru>
2025-06-22 16:34:57 +0300

    тесты

---
+++ Assets/Tests/GameTests.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UIElements;
+
+public class GameTests
+{
+    //[Test]
+    //public void GetTimeBySecondsTest()
+    //{
+    //    Assert.AreEqual("00:01:00", Translator.GetTimeBySeconds(60));
+    //}
+    //[Test]
+    //public void RemoveFoodDuplicatesTest()
+    //{
+    //    var recipeList = new List<SerializableFoodInfo>()
+    //    {
+    //        new SerializableFoodInfo(){ FoodId="Carrot", GramsWeight=50},
+    //        new SerializableFoodInfo(){ FoodId="Carrot", GramsWeight=75},
+    //        new SerializableFoodInfo(){ FoodId="Carrot", GramsWeight=100},
+    //        new SerializableFoodInfo(){ FoodId="Carrot", GramsWeight=100},
+    //        new SerializableFoodInfo(){ FoodId="Chicken", GramsWeight=100},
+    //        new SerializableFoodInfo(){ FoodId="Chicken", GramsWeight= 100},
+    //    };
+    //    var expectationList = new List<SerializableFoodInfo>()
+    //    {
+    //        new SerializableFoodInfo(){ FoodId="Carrot", GramsWeight=50},
+    //        new SerializableFoodInfo(){ FoodId="Carrot", GramsWeight=75},
+    //        new SerializableFoodInfo(){ FoodId="Carrot", GramsWeight=100},
+    //        new SerializableFoodInfo(){ FoodId="Chicken", GramsWeight=100},
+    //    };
+
+    //    Assert.AreEqual(expectationList, Recipe.RemoveDuplicates(recipeList));
+    //}
+    //[Test]
+    //public void GetDiffPercentTest()
+    //{
+    //    BellFinish bellFinish = new BellFinish();
+    //    Assert.AreEqual(66.6666641f, bellFinish.GetDiffPercent(200f, 100f));
+    //}
+}
